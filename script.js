921f5dbc2b62136a53bb849f87786aea23abf568
oussamaef06 <oussamaelfarihi@gmail.com>
2023-10-25 14:41:42 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function startSlideshow() {
+    let slideIndex = 0;
+    const slides = document.getElementsByClassName("auto-slider");
+  
+    function showSlides() {
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+  
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+  
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+  
+    showSlides(); // Initial call to start the slideshow
+  }
